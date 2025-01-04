8eb92182f3c6eecf76abc8ce2749459d92188335
pakkk <paco.benitez.chico@gmail.com>
2025-01-04 13:12:01 +0100

    Initial commit

---
+++ src/main/java/es/iesjandula/reaktor/base_client/utils/BaseClientConstants.java
@@ -0,0 +1,20 @@
+package es.iesjandula.reaktor.base_client.utils;
+
+/**
+ * @author Francisco Manuel Benítez Chico
+ */
+public class BaseClientConstants
+{
+	/*********************************************************/
+	/*********************** Errores *************************/
+	/*********************************************************/
+	
+	/** Error - Excepción genérica - Código */
+	public static final int ERR_GENERIC_EXCEPTION_CODE 	 		= 0 ;
+	
+	/** Error - Excepción genérica - Mensaje */
+	public static final String ERR_GENERIC_EXCEPTION_MSG 		= "Excepción genérica en " ;
+	
+	/** Error - Error mientras se obtenía el token personalizado JWT */
+	public static final int ERR_GETTING_PERSONALIZED_TOKEN_JWT  = 3 ;
+}
